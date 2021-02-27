a2c2866c4dfa1e894a7e7a40aef307e9a04001e4
Aaron Gonzalez <aarong.1503@gmail.com>
2021-02-27 16:19:30 -0500

    Styles fix

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
 let $hamburger = document.getElementById("hamburger");
 let $close = document.getElementById("close");
 let $mobModal = document.getElementById("mobModal");
+let $body = document.getElementById("body");
 
 const showModal = () => {
   $mobModal.style.display = "block";
   $close.style.display = "block";
   $hamburger.style.display = "none";
+  $body.style.overflowY = "hidden";
 };
 
 const hideModal = () => {
   $mobModal.style.display = "none";
   $close.style.display = "none";
   $hamburger.style.display = "block";
+  $body.style.overflowY = "visible";
 };
